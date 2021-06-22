7df378ea0a2b2c06e643cff7e010367c00093613
palerdos <palerdos95@gmail.com>
2021-06-22 09:56:51 +0200

    feat: add wait field to pagefactories and example wait method

--- src/test/java/pagefactory/JiraBrowsePage.java
+++ src/test/java/pagefactory/JiraBrowsePage.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class JiraBrowsePage {
     WebDriver drive;
+
+    WebDriverWait wait;
 
     @FindBy(id = "project-name-val")
     WebElement projectName;
--- src/test/java/pagefactory/JiraDashboardPage.java
+++ src/test/java/pagefactory/JiraDashboardPage.java
@@ -1,13 +1,20 @@
 package pagefactory;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class JiraDashboardPage {
     WebDriver driver;
+
+    WebDriverWait wait;
 
     @FindBy(id="create_link")
     WebElement createButton;
@@ -45,6 +52,11 @@ public class JiraDashboardPage {
     public JiraDashboardPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public void waitUntilProfileIsClickable() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(profileIcon));
     }
 
     public void clickOnCreateButton(){
