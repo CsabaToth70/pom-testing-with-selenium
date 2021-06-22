ad4a29b06aacecdf0dbe70a5b2e4be2892f6a9ea
palerdos <palerdos95@gmail.com>
2021-06-22 10:29:15 +0200

    feat: add class for testing login feature with successful login test completed

---
+++ src/test/java/test/TestJiraLogin.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pagefactory.JiraDashboardPage;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+public class TestJiraLogin {
+
+    WebDriver driver = new ChromeDriver();
+    JiraDashboardPage objDashboardPage;
+
+    @BeforeEach
+    void setUp() {
+        TestingHelper.setUp(driver);
+        objDashboardPage = new JiraDashboardPage(driver);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    void testSuccessfulLoginWithUserValidation() throws IOException {
+        objDashboardPage.sendKeysToUsernameField(TestingHelper.getProperty("username"));
+        objDashboardPage.sendKeysToPasswordField(TestingHelper.getProperty("password"));
+        objDashboardPage.clickOnLoginBtn();
+        objDashboardPage.waitUntilProfileIsClickable();
+        objDashboardPage.clickOnProfileIcon();
+        objDashboardPage.clickOnProfileOption();
+        assertEquals(TestingHelper.getProperty("username"), objDashboardPage.getUsernameText());
+    }
+}
